87da57b1af91612e17632a3dbf6301269e1dc4fb
Ed Singleton <singletoned@gmail.com>
2011-06-22 19:17:57 +0100

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 from setuptools import setup, find_packages
-import sys, os
-
-def read(*path):
-    """
-    Read and return content from ``path``
-    """
-    f = open(
-        os.path.join(
-            os.path.dirname(__file__),
-            *path
-        ),
-        'r'
-    )
-    try:
-        return f.read().decode('UTF-8')
-    finally:
-        f.close()
-
 
 setup(
-    name='flea',
-    version='6',
+    name='testino',
+    version='0.1',
     description="Test WSGI applications using lxml",
-    long_description=read('README'),
+    # long_description=read('README'),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -36,12 +18,12 @@ setup(
     author_email='oliver@redgecko.org',
     url='',
     license='BSD',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    py_modules=['testino'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pesto>=16',
         'lxml',
+        'werkzeug',
         # -*- Extra requirements: -*-
     ],
     entry_points="""
