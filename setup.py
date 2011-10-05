0d884b4ec9c3387f30fd3b2572a7f0974d77eca8
Ed Singleton <singletoned@gmail.com>
2011-10-05 19:11:19 +0100

    Hardened requirements

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'lxml',
-        'werkzeug',
+        'lxml>=2.3',
+        'werkzeug>=0.7',
         # -*- Extra requirements: -*-
     ],
     entry_points="""
