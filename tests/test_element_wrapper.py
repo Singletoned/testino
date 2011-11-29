6e32efad37f66b1f18e041c30ff07b44c48a5426
Ed Singleton <singletoned@gmail.com>
2011-11-29 10:12:26 +0000

    Mock out xpath_funcs

--- tests/test_element_wrapper.py
+++ tests/test_element_wrapper.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import mock
+
 import testino as t
 
 def test_xpath_func():
-    @t.xpath_func("foo")
-    def bar(element):
-        return "flibble"
+    with mock.patch.object(t, 'xpath_funcs', dict()):
+        @t.xpath_func("foo")
+        def bar(element):
+            return "flibble"
 
-    assert t.xpath_funcs == dict(bar=[("foo", bar)])
+        assert t.xpath_funcs == dict(bar=[("foo", bar)])
+
 
 def test_element_wrapper_getattr():
-    @t.xpath_func("foo")
-    def bar(element):
-        return "flibble"
+    with mock.patch.object(t, 'xpath_funcs', dict()):
+        @t.xpath_func("foo")
+        def bar(element):
+            return "flibble"
 
-    assert t._ElementWrapper("foobar").bar() == "flibble"
+        assert t._ElementWrapper("foobar").bar() == "flibble"
+
 
