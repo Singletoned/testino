3dcc2f78b8fc57edd2e6c25842289b2b79040e9e
Ed Singleton <singletoned@gmail.com>
2017-04-21 11:35:45 +0100

    Moar tests

--- tests/test_testino.py
+++ tests/test_testino.py
@@ -27,10 +27,20 @@ class TestAgent(unittest.TestCase):
     def setUp(self):
         self.response = Response(StubResponse(document))
 
-    def test_has_one(self):
+    def test_one(self):
         el = self.response.one("div#foo")
         assert el.text_content().strip() == "This is foo"
 
-    def test_has_one_fails(self):
+    def test_one_fails(self):
         with nose.tools.assert_raises(AssertionError):
             self.response.one("div#fumble")
+
+    def test_has_one(self):
+        assert self.response.has_one("div#foo")
+
+    def test_has_one_fails(self):
+        assert not self.response.has_one("div#fumble")
+
+    def test_all(self):
+        els = self.response.all("div")
+        assert len(els) == 2
