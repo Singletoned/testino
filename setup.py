cedf658620a401c677adddc0c3f9f5ff8d5aa9fc
Ed Singleton <singletoned@gmail.com>
2021-03-25 23:07:43 +0000

    Up version to 0.3.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 data = dict(
     name='testino',
-    version='0.3.10',
+    version='0.3.11',
     description="Test WSGI applications using lxml",
     long_description="",
     classifiers=[
